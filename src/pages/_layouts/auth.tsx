3a6ba832e62a33cc7cd1865ccf627ef3b51be732
Marcos Paulo <marcospaullo552@gmail.com>
2024-04-26 16:40:45 -0300

    feat: sign in, sign up pages

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,5 +1,24 @@
+import { Pizza, PlaySquare } from 'lucide-react'
+import { Outlet } from 'react-router-dom'
+
 export function AuthLayout() {
   return (
-    <h1>Layout Auth</h1>
+    <div className="container relative hidden min-h-screen flex-col items-center justify-center antialiased md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative hidden h-full flex-col border-r border-zinc-500 bg-zinc-900 p-10 dark:border-r lg:flex">
+        <div className="flex items-center gap-3 text-lg font-medium">
+          <PlaySquare className="h-5 w-5" />
+          <span className="font-semibold">upload.video</span>
+        </div>
+        <div className="mt-auto">
+          <footer className="text-sm text-zinc-500">
+            Upload de vídeos &copy; <strong>upload.video</strong> - {new Date().getFullYear()}
+          </footer>
+        </div>
+      </div>
+
+      <div className="relative bg-zinc-950 flex min-h-screen flex-col items-center justify-center">
+        <Outlet />
+      </div>
+    </div>
   )
 }
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,5 +1,63 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { twMerge } from "tailwind-merge";
+
 export function SignUp() {
   return (
-    <h1>Signup</h1>
+    <div className="lg:p-8 text-zinc-50">
+      <a
+        href="/sign-in"
+        className={twMerge(
+          buttonVariants({ variant: 'ghost' }),
+          'absolute right-4 top-4 md:right-8 md:top-8',
+        )}
+      >
+        Entrar
+      </a>
+
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Criar conta grátis
+          </h1>
+          <p className="text-sm text-gray-500">
+            Entre e aproveite as vantagens do <strong>upload.video</strong>
+          </p>
+        </div>
+
+        <div className="grid gap-6">
+          <form>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Seu nome</Label>
+                <Input
+                  id="name"
+                  type="text"
+                  autoCapitalize="none"
+                  autoComplete="name"
+                  autoCorrect="off"
+                />
+              </div>
+              
+              <div className="grid gap-2">
+                <Label htmlFor="email">Seu e-mail</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  autoCapitalize="none"
+                  autoComplete="email"
+                  autoCorrect="off"
+                />
+              </div>
+
+              <Button type="submit">
+                Finalizar cadastro
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
